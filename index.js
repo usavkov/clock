e55f97e5d834a082e509ece90ff26e6d77e6f702
Yuri Savkov <yury.saukou@gmail.com>
2021-02-05 20:46:45 +0300

    init: innit commit

---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const watchesLoaded = new Promise((res, rej) => {
+    document.querySelector('#watches').addEventListener('load', (event) => {
+        res(event.target.contentDocument.querySelector('svg'));
+    })
+})
+
+watchesLoaded.then((svg) => {
+    const hourHand = svg.querySelector('#hourHand');
+    const minHand = svg.querySelector('#minHand');
+    const secHand = svg.querySelector('#secHand');
+
+    function setHands() {
+        const curDate = new Date();
+        
+        const curHours = curDate.getHours() > 12 ? curDate.getHours() - 12 : curDate.getHours();
+        const hDeg = curHours / 12 * 360;
+
+        if (curHours === 0) {
+            (function throwSec() {
+                secHand.style.transitionDuration = '0s';
+                setTimeout(() => {
+                    secHand.style.transitionDuration = '.25s';
+                })
+            })()
+        }
+
+        const curMinutes = curDate.getMinutes();
+        const mDeg = curMinutes / 60 * 360;
+
+        if (curMinutes === 0) {
+            (function throwMin() {
+                minHand.style.transitionDuration = '0s';
+                setTimeout(() => {
+                    minHand.style.transitionDuration = '.25s';
+                })
+            })()
+        }
+
+        const curSeconds = curDate.getSeconds();
+        const sDeg = curSeconds / 60 * 360;
+
+        if (curSeconds === 0) {
+            (function throwSec() {
+                secHand.style.transitionDuration = '0s';
+                setTimeout(() => {
+                    secHand.style.transitionDuration = '.25s';
+                })
+            })()
+        }
+
+        secHand.style.transform = `rotate(${sDeg}deg)`;
+        minHand.style.transform = `rotate(${mDeg}deg)`;
+        hourHand.style.transform = `rotate(${hDeg}deg)`;
+    }
+
+    setInterval(setHands, 1000);
+})
